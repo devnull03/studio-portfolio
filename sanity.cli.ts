609f15ebb12c1a4c4930e3a094e5787e18bc7048
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-08-08 19:13:36 -0700

    config: added studioHost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'wt0c9f6z',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'dvnl'
 })
